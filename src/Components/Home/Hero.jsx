21de470038adef02842e110673d7675daeb25323
Dhaya3132 <dhayalanu103@gmail.com>
2024-08-24 18:57:50 +0530

    feat - added responsive for hero section

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -2,19 +2,19 @@ import hero_icon from '../../assets/hero_icon.svg';
 import upArrow_icon from '../../assets/upArrow_icon.svg';
 const Hero = () => {
     return (
-        <section className='bg-bgBanner w-full h-full p-5'>
-            <div className="p-5 flex justify-center items-center gap-5 mt-10">
-                <div className='w-1/2' id="leftPart">
-                    <h2 className="font-semibold text-7xl text-black">Discover new Collection</h2>
+        <section className='md:bg-bgBanner bg-white w-full h-full p-5'>
+            <div className="p-5 flex md:flex-row flex-col justify-center items-center gap-5 mt-10">
+                <div className='md:w-1/2 w-full' id="leftPart">
+                    <h2 className="font-semibold md:text-7xl text-4xl text-black">Discover new Collection</h2>
                     <p className="font-light text-gray-500 mt-1">Fashion is part of the daily air and it changes all the time, with all the events.</p>
                     <button className="px-12 py-2 text-white font-normal bg-Vermillion mt-4 flex items-center gap-5">Start shopping <img src={upArrow_icon} alt="uparrow" className='w-3 h-3' /></button>
                     <ul className="flex gap-5 font-Poppins mt-5">
-                        <li className='border-r-4 border-slate-200 pr-2'><h3 className="font-bold text-3xl">200+</h3><p className="font-light text-medium text-lg">happy Customer</p></li>
-                        <li className='border-r-4 border-slate-200 pr-3'><h3 className="font-bold text-3xl">1000+</h3><p className="font-light text-medium text-lg ">Quality products</p></li>
-                        <li><h3 className="font-bold text-3xl">100+</h3><p className="font-light text-medium text-lg">international brands</p></li>
+                        <li className='border-r-4 border-slate-200 pr-2'><h3 className="font-bold md:text-3xl text-xl">200+</h3><p className="font-light text-medium md:text-lg text-base">happy Customer</p></li>
+                        <li className='border-r-4 border-slate-200 pr-3'><h3 className="font-bold md:text-3xl text-xl">1000+</h3><p className="font-light text-medium md:text-lg text-base">Quality products</p></li>
+                        <li><h3 className="font-bold md:text-3xl text-xl">100+</h3><p className="font-light text-medium md:text-lg text-base">international brands</p></li>
                     </ul>
                 </div>
-                <div className='w-1/4 h-1/4' id='rightPart'>
+                <div className='md:w-1/4 md:h-1/4 w-full h-full' id='rightPart'>
                     <img src={hero_icon} alt="hero_icon" className='w-full h-full' />
                 </div>
             </div>
