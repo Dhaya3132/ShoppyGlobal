d15d7b6ddde7eed45586c7b20a2c84b8c27f331e
Dhaya3132 <dhayalanu103@gmail.com>
2024-08-24 21:12:33 +0530

    feat - added responsive for prdouctdetails

--- src/Components/ProductDetail.jsx
+++ src/Components/ProductDetail.jsx
@@ -9,47 +9,45 @@ const ProductDetail = () => {
 
     return (
 
-        <div><div className='w-full p-2 flex flex-col md:flex-row md:justify-center gap-5 mt-10'>
-            <div className='w-3/12 bg-sky-50 border-2 border-sky-200 rounded-sm p-5'>
-                <img src={productData.images} alt="product_image" className='w-full h-full object-cover' />
+        <div>
+            <div className='w-full md:p-2 flex flex-col md:flex-row md:justify-center justify-center items-center p-5 gap-5 mt-10'>
+                <div className='md:w-3/12 w-4/5 flex flex-shrink-0 bg-sky-50 border-2 border-sky-200 rounded-sm p-5'>
+                    <img src={productData.images} alt="product_image" className='w-full h-full object-contain' />
+                </div>
+                <div className='md:w-1/2 w-full '>
+                    <h2 className='font-medium md:text-4xl text-3xl mb-1'>{productData.title}</h2>
+                    <p className='font-light text-base text-gray-400'>{productData.description}</p>
+                    <div className='flex items-center gap-2 mt-2'>
+                        <p className='font-semibold text-3xl'>${productData.price}</p>
+                        <p className='bg-green-100 border-2 border-green-200 text-xs font-base px-3 py-1 rounded-full'>Off {productData.discountPercentage}%</p>
+                    </div>
+                    <div className='flex items-center gap-5 mt-4'>
+                        <button className='px-6 py-1.5 text-white font-medium bg-black'>Add to cart</button>
+                        <button className='px-6 py-1.5 text-black font-medium border-2 border-black'>Wishlist</button>
+                    </div>
+                    <div className='w-fit mt-5'>
+                        <ul className='flex items-center'>
+                            <li className='border-2 border-gray-200 p-3 md:text-lg text-sm'>{productData.warrantyInformation}</li>
+                            <li className='border-2 border-gray-200 p-3 md:text-lg text-sm'>{productData.shippingInformation}</li>
+                            <li className='border-2 border-gray-200 p-3 md:text-lg text-sm'>{productData.returnPolicy}</li>
+                        </ul>
+                    </div>
+                    <section className='bg-gray-100 mt-6 p-5'>
+                        <h2 className='font-medium text-black text-lg'>Product Information</h2>
+                        <ul id="about" className='mt-3'>
+                            <li className='text-black text-sm'>Brand : {productData.brand}</li>
+                            <li className='text-black text-sm'>Minimum Order Quantity : {productData.minimumOrderQuantity}</li>
+                            <li className='text-black text-sm'>In Stock : {productData.stock}</li>
+                            <li className='text-black text-sm'>Weight : {productData.weight}gms</li>
+                            <li className='text-black text-sm'>
+                                Dimensions:
+                                {productData?.dimensions ? ` ${productData.dimensions.width} x ${productData.dimensions.height} x ${productData.dimensions.depth}`: 'N/A'}
+                            </li>
+                        </ul>
+                    </section>
+                </div>
             </div>
-            <div className='w-1/2'>
-                <h2 className='font-medium text-4xl mb-1'>{productData.title}</h2>
-                <p className='font-light text-base text-gray-400'>{productData.description}</p>
-                <div className='flex items-center gap-2 mt-2'>
-                    <p className='font-semibold text-3xl'>${productData.price}</p>
-                    <p className='bg-green-100 border-2 border-green-200 text-xs font-base px-3 py-1 rounded-full'>Off {productData.discountPercentage}%</p>
-                </div>
-                <div className='flex items-center gap-5 mt-4'>
-                    <button className='px-6 py-1.5 text-white font-medium bg-black'>Add to cart</button>
-                    <button className='px-6 py-1.5 text-black font-medium border-2 border-black'>Wishlist</button>
-                </div>
-                <div className='w-fit mt-5'>
-                    <ul className='flex items-center'>
-                        <li className='border-2 border-gray-200 p-3 '>{productData.warrantyInformation}</li>
-                        <li className='border-2 border-gray-200 p-3'>{productData.shippingInformation}</li>
-                        <li className='border-2 border-gray-200 p-3'>{productData.returnPolicy}</li>
-                    </ul>
-                </div>
-                <section className='bg-gray-100 mt-6 p-5'>
-                    <h2 className='font-medium text-black text-lg'>Product Information</h2>
-                    <ul id="about" className='mt-3'>
-                        <li className='text-black text-sm'>Brand : {productData.brand}</li>
-                        <li className='text-black text-sm'>Minimum Order Quantity : {productData.minimumOrderQuantity}</li>
-                        <li className='text-black text-sm'>In Stock : {productData.stock}</li>
-                        <li className='text-black text-sm'>Weight : {productData.weight}gms</li>
-                        <li className='text-black text-sm'>
-                            Dimensions:
-                            {productData?.dimensions
-                                ? ` ${productData.dimensions.width} x ${productData.dimensions.height} x ${productData.dimensions.depth}`
-                                : 'N/A'}
-                        </li>
-                    </ul>
-                </section>
-            </div>
-        </div>
-        <ProductReview productData={productData}/>
-
+            <ProductReview productData={productData} />
         </div>
     )
 }
--- src/Components/ProductReview.jsx
+++ src/Components/ProductReview.jsx
@@ -6,17 +6,17 @@ const ProductReview = ({productData}) => {
     return (
         <div className='mt-5 p-5'>
             <h2 className='font-semibold text-center text-3xl'>Reviews</h2>
-            <ul className="flex justify-center items-center gap-5 mt-5">
+            <ul className="flex md:flex-row flex-col justify-center items-center gap-5 mt-5">
                 {productData?.reviews?.length > 0 ? (
                     productData.reviews.map((productData) => (
                         <li className="p-5 border-2 border-black text-black shadow-lg" key={nanoid()}>
-                            <div className="flex items-center  gap-1 bg-black text-white float-right px-3 py-1 rounded-full">
+                            <div className="flex items-center gap-1 bg-black text-white float-right px-3 py-1 rounded-full">
                                 <img src={star_icon} alt="Rating Star" className="w-4 h-4" />
                                 <p className='text-xs'>{productData.rating}</p>
                             </div>
                             <div className="mt-10 text-black">
-                                <h2 className="font-medium text-lg text-gray-500">{productData.reviewerName}</h2>
-                                <p className="font-semibold text-2xl">{productData.comment}</p>
+                                <h2 className="font-medium md:text-lg text-base text-gray-500">{productData.reviewerName}</h2>
+                                <p className="font-semibold md:text-2xl text-lg">{productData.comment}</p>
                             </div>
                         </li>
                     ))
