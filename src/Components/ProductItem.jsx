84bc6846918f123e4b46b9f1299806ccc29aab2e
Dhaya3132 <dhayalanu103@gmail.com>
2024-08-30 20:51:47 +0530

    feat - bug fix

--- src/Components/ProductItem.jsx
+++ src/Components/ProductItem.jsx
@@ -14,9 +14,9 @@ const ProductItem = ({ product }) => {
                     <div className='bg-LightGreen flex justify-center rounded-lg'><img src={product.images} alt="product_image" className="w-56 h-56 rounded-lg" /></div>
                     <h2 className='font-semibold text-lg mt-1'>{product.title.length >= 12 ? product.title.slice(0, 15) + '...' : product.title}</h2>
                     <p className='font-semibold text-base mt-1 text-Vermillion'>${product.price}</p>
-                    <button className="bg-blue-500 px-6 py-2 mt-2" onClick={handleCart}>Add to Cart</button>
                 </div>
             </Link>
+            <button className="bg-blue-500 text-white px-4 py-1 mt-2" onClick={handleCart}>Add to Cart</button>
         </div>
     )
 }
