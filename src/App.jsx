95d7bb97adb6f5b2b40e7cc19209d8955af869ab
Dhaya3132 <dhayalanu103@gmail.com>
2024-09-15 19:13:00 +0530

    feat - login and sign up page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { useState } from 'react';
 import Header from './Components/Header';
 import { Outlet } from 'react-router-dom';
+import LoginPopUp from './Components/LoginPopUp';
 function App() {
+  const [login, setLogin] = useState(true);
   return (
     <>
+      { login ? <LoginPopUp /> : <></> }
       <Header />
       <Outlet />
     </>
---
+++ src/Components/LoginPopUp.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const LoginPopUp = () => {
+    const [currentState, setCurrentState] = useState('Sign In');
+    const navigate = useNavigate()
+    const [data, setData] = useState({
+        name:"",
+        email:"",
+        password:"",
+    })
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setData({
+            ...data,
+            [name]:value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(data)
+    }
+  return (
+    <div className='z-50 fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center' onSubmit={handleSubmit}>
+        <form className='bg-white p-10 w-96 rounded-lg'>
+            <h2 className='font-semibold text-center text-lg mb-2'>{currentState}</h2>
+            {currentState === 'Sign In' && <div className='flex flex-col gap-1'>
+                <h2 className='font-medium'>Name</h2>
+                <input type="text" name="name" value={data.name} onChange={handleChange} placeholder='Enter your Name' className='p-2 border-2 border-slate-200 rounded-md w-full ' />
+            </div>}
+            <div className='flex flex-col gap-1 mt-2'>
+                <h2 className='font-medium'>Email</h2>
+                <input type="email" name="email" value={data.email} onChange={handleChange} placeholder='Enter your email' className='p-2 border-2 border-slate-200 rounded-md w-full '  />
+            </div>
+            <div className='flex flex-col gap-1 mt-2'>
+                <h2 className='font-medium'>Password</h2>
+                <input type="password" name='password' value={data.password} onChange={handleChange} placeholder='Enter your password' className='p-2 border-2 border-slate-200 rounded-md w-full '  />
+            </div>
+            <button className='w-full p-2 bg-green-800 text-white mt-5 rounded-md'>Login</button>
+            <div className='mt-5'>
+                {currentState === 'Sign In' && <p className='font-light text-sm'>Already have an account? <span className='text-blue-500' onClick={()=> setCurrentState('Login')} >Login</span></p>}
+                {currentState === 'Login' &&<p className='font-light text-sm'>Don't have an account? <span className='text-blue-500' onClick={()=> setCurrentState('Sign In')}>Sign In</span></p>}
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default LoginPopUp;
